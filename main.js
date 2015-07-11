66f390612ec12c4a3333c132f6671e3a7f479044
KyleRattet <krattet@gmail.com>
2015-07-10 22:04:57 -0600

    built main skeleton, need to iron out some kinks still

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 var phoneNumber = prompt("Enter your phone number with dashes (i.e. XXX-XXX-XXXX)");
 
-if (phoneNumber.charAt(3&7)==="-"){
-  console.log("True");
-}
-else {
-  console.log("False");
-}
+  if (phoneNumber.charAt(3&7)==="-"){
+    alert("Thanks!");
+  }
+  else {
+    alert("False. Incorrect phone number format. Please try again.");
+  }
+
+var birthDate = prompt("Enter your birth day (i.e. xx/xx/xx)");
+
+  if (birthDate.charAt(2&5)==="/"){
+    alert("Gracias");
+  }
+  else {
+    alert("Wrong format. Please try again in the correct xx/xx/xx format");
+  }
+
+
+var postal = prompt("What is your postal code? (i.e. XXXXX)");
+  if (postal.length===5){
+    alert("Thx");
+  }
+  else {
+    alert("Wrong format. Please try again in the correct XXXXX format");
+  }
+
+var stateAbbr = prompt("What state abbreviation? (i.e. XX)");
+    if (stateAbbr.length===2){
+      alert("Thank you.");
+    }
+    else {
+      alert("Wrong format. Please try again");
+    }
+
+var married = prompt("Married? Yes or No?");
+    if (married==="Yes"){
+      alert("Congrats!");
+    }
+    else if(married==="No"){
+      alert("Maybe later, eh?");
+    }
+    else {
+      alert("Wrong format. Please re-enter");
+    }
